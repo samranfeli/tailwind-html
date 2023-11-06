5464d3722740536d11a535b49477fc8acc04eddf
s.feli <s.feli@DESKTOP-8HQAAOS>
2023-11-06 22:14:43 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'yekan': ['Yekan', 'sans-serif']
+      },
+      colors: {
+        blue: {
+          50: '#f4f4fb',
+          100: '#e6e7fe',
+          200: '#cecefd',
+          300: '#b5b6fd',
+          400: '#8385fb',
+          500: '#5255fa',
+          600: '#2e31f8',
+          700: '#070adf',
+          800: '#0508ad',
+          900: '#04067c',
+          950: '#02034a',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
+
